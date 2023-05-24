f8b309343228eb1dbef8e4ae90fe02c6e310b2db
zrakib-dev <dev.zrakib@gmail.com>
2023-05-24 10:30:07 +0600

    1 module added

---
+++ apply-js/sum-total-array.js
@@ -0,0 +1,10 @@
+function getSumTotal(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    const index = i;
+    const element = numbers[index];
+    console.log(index);
+  }
+}
+
+const arr = [52, 85, 65, 47, 36, 98, 58, 85, 74, 63, 78];
+getSumTotal(arr);
