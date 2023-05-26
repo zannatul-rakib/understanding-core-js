2e8f9fd121cd9d99f7e50e0f30e6dc1f452205cd
zrakib-dev <dev.zrakib@gmail.com>
2023-05-26 14:32:28 +0600

    simple problem solving start

---
+++ simple-problem-solving/array.js
@@ -0,0 +1,24 @@
+const country = "Bangladesh";
+const age = 53;
+const isIndependent = true;
+const student = {
+  name: "Kuddus",
+  age: 17,
+  class: 11,
+  section: "science",
+};
+const friendsAge = [23, 21, 14, 16, 19, 17];
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof add);
+// check array using Array.isArray
+console.log(Array.isArray(friendsAge));
+
+// using 'includes' find something in array
+console.log(friendsAge.includes(21));
